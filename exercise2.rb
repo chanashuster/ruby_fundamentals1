00c8ac9496090e53fcdd9352958d449306fbec6b
chanashuster <chana.shuster@gmail.com>
2018-12-12 22:23:26 -0500

    tips&multiply

---
+++ exercise2.rb
@@ -0,0 +1,19 @@
+#Tips for meal
+mealCost= 55
+tipsPercent=0.10
+
+totalTip=mealCost*tipsPercent
+totalCost=mealCost+totalTip
+
+puts "Meal=$55"
+puts "Tip= $#{totalTip}"
+puts "Total cost=$#{totalCost}"
+
+
+
+#Multiplication using string interpolation
+
+a=45628
+b=7839
+
+puts "#{a*b}"
