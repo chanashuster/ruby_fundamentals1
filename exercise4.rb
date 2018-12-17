08bc652adc71fce6b515e3c217beafdeccf5865d
chanashuster <chana.shuster@gmail.com>
2018-12-17 17:46:07 -0500

    how old, years apart complete!

--- exercise4.rb
+++ exercise4.rb
@@ -11,12 +11,14 @@ end
 puts "How old are you?"
 age=gets.chomp.to_i
 
-if age>19
+if age>19 && age<105
     apart=age-19
     puts "We are #{apart} years apart"
 elsif age<19
 apart=19-age
 puts "We are #{apart} years apart"
+elsif age>=105
+    puts "I'm not sure I believe you"
 else
     puts "We are the same age"
 end
