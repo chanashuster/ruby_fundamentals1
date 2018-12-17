76997bc1576e6d1ee557f120c2754be781c47435
chanashuster <chana.shuster@gmail.com>
2018-12-17 17:41:05 -0500

    big num, years apart

---
+++ exercise4.rb
@@ -0,0 +1,22 @@
+  #That's a big number
+puts "Enter a number"
+number=gets.chomp.to_i
+
+if number>100
+    puts "That's a big number"
+end
+
+
+  #Years apart
+puts "How old are you?"
+age=gets.chomp.to_i
+
+if age>19
+    apart=age-19
+    puts "We are #{apart} years apart"
+elsif age<19
+apart=19-age
+puts "We are #{apart} years apart"
+else
+    puts "We are the same age"
+end
